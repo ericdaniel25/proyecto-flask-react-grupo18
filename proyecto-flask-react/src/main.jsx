134618b13e65d75c0ff93a33ee70645e3e05f731
ericdaniel25 <150204656+ericdaniel25@users.noreply.github.com>
2024-11-14 21:57:54 -0400

    DelateUsers
    lista para eliminar los usuarios

--- proyecto-flask-react/src/main.jsx
+++ proyecto-flask-react/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.css'
-import App from './App.jsx'
-
+/* import App from './App.jsx' */
+import DelateUsers from './pages/DelateUsers.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <DelateUsers/>
   </StrictMode>,
 )
